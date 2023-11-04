859e42648ade480a13932ca1e847fd362d70c6d2
PedroVictorMcB <pedrovictormcb80@gmail.com>
2023-11-04 12:24:09 -0300

    Trabalho concluido

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+//Funções
+const soma = function(a, b) {
+    return a + b;
+}
+
+const minus = function(a, b) {
+    return a - b;
+}
+
+const divide = function(a, b) {
+    if (b == 0) {
+        console.log("Cannot operate, b is equal to 0");
+    }
+    return a / b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+app.get('/', function(req, res) {
+    res.send('Oi, mundo :-)');
+  });
+
+//request POST
+app.post("/soma", function(req, res) { 
+    const body = req.body;
+    const result = soma(body.a, body.b);
+    
+    const num1 = req.body.a; 
+    const num2 = req.body.b;   
+    res.send(`O resultado da soma de ${body.a} + ${body.b} é ${result}`); 
+});
+
+app.post("/subtracao", function(req, res) { 
+    const body = req.body;
+    const result = minus(body.a, body.b);
+    
+    const num1 = req.body.a; 
+    const num2 = req.body.b;   
+    res.send(`O resultado da subtração de ${body.a} - ${body.b} é ${result}`); 
+});
+
+app.post("/divisao", function(req, res) { 
+    const body = req.body;
+    const result = divide(body.a, body.b);
+    
+    const num1 = req.body.a; 
+    const num2 = req.body.b;   
+    res.send(`O resultado da divisão de ${body.a} / ${body.b} é ${result}`); 
+});
+
+app.post("/multiplicacao", function(req, res) { 
+    const body = req.body;
+    const result = multiply(body.a, body.b);
+    
+    const num1 = req.body.a; 
+    const num2 = req.body.b;   
+    res.send(`O resultado da multiplicaçao de ${body.a} x ${body.b} é ${result}`); 
+});
+
+const port = 3000;
+
+app.listen(port, function(erro) {
+    if (erro) {
+        console.log("unable to initialize");
+    } else {
+        console.log(`Server initialized in http://localhost:${port}/`);
+    }
+})
